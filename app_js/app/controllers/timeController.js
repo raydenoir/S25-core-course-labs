327e4aa8e1afcf51a8627d99b41d258c6f6fb5d3
Nikita Sannikov <54597924+raydenoir@users.noreply.github.com>
2025-01-28 13:18:58 +0300

    Revert "lab1 + bonus"

--- app_js/app/controllers/timeController.js
+++
@@ -1,8 +0,0 @@
-const { getCurrentMoscowTime } = require('../utils/timeUtils');
-
-const getMoscowTime = (req, res) => {
-  const moscowTime = getCurrentMoscowTime();
-  res.render('moscow_time', { time: moscowTime });
-};
-
-module.exports = { getMoscowTime };
--- app_js/app/routes/timeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const timeController = require('../controllers/timeController');
-
-// Endpoint to render Moscow time
-router.get('/', timeController.getMoscowTime);
-
-module.exports = router;
